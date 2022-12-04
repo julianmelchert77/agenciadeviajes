8d72e5cff1c73ff6fe482d7f2e187f7a4693b15b
Julian Melchert <julianmelchert77@gmail.com>
2022-12-03 21:00:44 -0300

    Proyecto con Express

---
+++ index.js
@@ -0,0 +1,42 @@
+import express, { application } from 'express';
+import router from './routes/index.js';
+import db from './config/db.js';
+
+console.log(process.env.DB_HOST)
+
+const app = express();
+
+// Conectar la base de datos
+db.authenticate()
+  .then(() => console.log('Base de datos conectada'))
+  .catch( error => console.log(error));
+
+// Definir puerto
+const port = process.env.PORT || 4000;
+
+// Habilitar PUG
+app.set('view engine', 'pug');
+
+// Obtener el año actual
+app.use( (req, res, next) => {
+  const year = new Date();
+
+  res.locals.actualYear = year.getFullYear();
+  res.locals.nombreSitio = 'Agencia de Viajes';
+
+   next();
+});
+
+// Agregar body parser para leer los datos del formulario
+app.use(express.urlencoded({extended: true}));
+
+// Definir la carpeta publica
+app.use(express.static('public'));
+
+// Agregar Router
+app.use('/', router);
+
+
+app.listen(port, () => {
+    console.log(`El Servidor está funcionando en el puerto ${port}`)
+});
